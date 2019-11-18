b1d560458d3c8add1c57f193dcb48d986deaa8db
narendra.pathai <narendra.pathai@gmail.com>
2019-11-19 00:58:51 +0530

    Added support for addition of two comma separated numbers

--- src/test/java/com/npathai/calculator/StringCalculatorTest.java
+++ src/test/java/com/npathai/calculator/StringCalculatorTest.java
@@ -27,13 +27,28 @@ public class StringCalculatorTest {
         assertThat(sum, is(6));
     }
 
+    @Test
+    public void returnsSumOfTwoCommaSeparatedNumbers() {
+        StringCalculator calculator = new StringCalculator();
+
+        int sum = calculator.add("1,2");
+        assertThat(sum, is(3));
+        sum = calculator.add("5,2");
+        assertThat(sum, is(7));
+    }
+
     private class StringCalculator {
 
         public int add(String input) {
             if (input.isEmpty()) {
                 return 0;
             }
-            return Integer.parseInt(input);
+            String[] summands = input.split(",");
+            if (summands.length == 1) {
+                return Integer.parseInt(summands[0]);
+            } else {
+                return Integer.parseInt(summands[0]) + Integer.parseInt(summands[1]);
+            }
         }
     }
 }
