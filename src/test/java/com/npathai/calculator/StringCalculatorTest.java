b06b75f5d159e1e67c879a009572288f888e2ab8
narendra.pathai <narendra.pathai@gmail.com>
2019-11-19 00:44:42 +0530

    Added support for addition of single number

--- src/test/java/com/npathai/calculator/StringCalculatorTest.java
+++ src/test/java/com/npathai/calculator/StringCalculatorTest.java
@@ -14,12 +14,26 @@ public class StringCalculatorTest {
         int sum = calculator.add("");
         assertThat(sum, is(0));
     }
-    
+
+    @Test
+    public void sumOfSingleNumberIsNumberItself() {
+        StringCalculator calculator = new StringCalculator();
+
+        int sum = calculator.add("4");
+        assertThat(sum, is(4));
+        sum = calculator.add("5");
+        assertThat(sum, is(5));
+        sum = calculator.add("6");
+        assertThat(sum, is(6));
+    }
 
     private class StringCalculator {
 
         public int add(String input) {
-            return 0;
+            if (input.isEmpty()) {
+                return 0;
+            }
+            return Integer.parseInt(input);
         }
     }
 }
